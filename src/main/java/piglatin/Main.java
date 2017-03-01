d0ac3bb88bb0f118e0d67fd2fae5418229791001
Per Skarin <per.skarin@control.lth.se>
2017-03-01 08:49:22 +0000

    Setup gradle for stdin and replace Console with BufferedReader

--- src/main/java/piglatin/Main.java
+++ src/main/java/piglatin/Main.java
@@ -1,13 +1,16 @@
 package piglatin;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         PigLatinTranslator plt = new PigLatinTranslator();
         System.out.println("Enter english phrase to translate: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
-            String src = System.console().readLine();
+            String src = reader.readLine();
             if (src.isEmpty()) {
                 break;
             }
