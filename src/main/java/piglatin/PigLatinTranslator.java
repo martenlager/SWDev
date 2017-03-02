3c8c1f6187269b49dcd96e28640941841a0dd67f
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-03-02 12:49:32 +0100

    added rule1 based translation, which is the most basic rule.

--- src/main/java/piglatin/PigLatinTranslator.java
+++ src/main/java/piglatin/PigLatinTranslator.java
@@ -2,6 +2,10 @@ package piglatin;
 
 public class PigLatinTranslator {
     public String translate(String src) {
-        return src;
+        if (src == null || src.isEmpty()) {
+            return src;
+        }
+        String lc = src.toLowerCase();
+        return lc.substring(1, 2).toUpperCase() + lc.substring(2) + lc.substring(0, 1) + "ay";
     }
 }
--- src/test/java/piglatin/PigLatinTranslatorTest.java
+++ src/test/java/piglatin/PigLatinTranslatorTest.java
@@ -5,15 +5,30 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PigLatinTranslatorTest {
+
     @Test
     public void emptyWord() {
         PigLatinTranslator plt = new PigLatinTranslator();
         assertEquals("", plt.translate(""));
+        assertEquals(null, plt.translate(null));
     }
-    
+
     @Test
-    public void world() {
+    public void rule1() {
         PigLatinTranslator plt = new PigLatinTranslator();
-        assertEquals("Hello World!", plt.translate("Hello World!"));
+        String[][] pairs = new String[][]{
+                new String[]{"pig", "Igpay"},
+                new String[]{"Latin", "Atinlay"},
+                new String[]{"banana", "Ananabay"},
+                new String[]{"happy", "Appyhay"},
+                new String[]{"duck", "Uckday"},
+                new String[]{"latin", "Atinlay"},
+                new String[]{"dopest", "Opestday"},
+                new String[]{"me", "Emay"},
+                new String[]{"too", "Ootay"},
+        };
+        for (String[] pair : pairs) {
+            assertEquals(pair[1], plt.translate(pair[0]));
+        }
     }
 }
