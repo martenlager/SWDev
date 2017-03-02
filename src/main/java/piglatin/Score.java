0c334bc740fe0b3be7999b174846c6ea18f649e7
Per Skarin <per.skarin@control.lth.se>
2017-03-02 12:04:25 +0000

    score: add accumulated score

--- src/main/java/piglatin/Score.java
+++ src/main/java/piglatin/Score.java
@@ -1,9 +1,26 @@
 package piglatin;
 
 class Score {
+  private int sum = 0;
+
   Score() {}
 
+  /**
+    Scores a string.
+    Returns the single score but also sums up internally.
+    @see getScore
+  */
   public int score(final String solution, final String answer) {
-    return solution.equals(answer) ? 1 : 0;
+    int v = solution.equals(answer) ? 1 : 0;
+    sum += v;
+    return v;
+  }
+
+ 
+  /**
+    Get accumulated score
+  */ 
+  public int getScore() {
+    return sum;
   }
 }
