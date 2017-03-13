4ad68500cf5d5ddbe1a8e0d548af599b09a40cc9
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-03-13 10:55:56 +0100

    minor cleanups

--- src/main/java/piglatin/SentenceReader.java
+++ src/main/java/piglatin/SentenceReader.java
@@ -4,14 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SentenceReader {
 
     public List<String> read(InputStream stream) throws IOException {
-        List<String> sentences = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
             return br.lines().collect(Collectors.toList());
         }
