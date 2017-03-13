cc012cc05d22396d50170751da33b6a9d7ddc0bd
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-03-13 09:07:56 +0100

    using java stream api

--- src/main/java/piglatin/PigLatinSentenceTranslator.java
+++ src/main/java/piglatin/PigLatinSentenceTranslator.java
@@ -1,48 +1,18 @@
 package piglatin;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class PigLatinSentenceTranslator {
-    public String translateSentence(String src) {
-        if (src == null) {
-            throw new NullPointerException();
+    String translateSentence(String src) {
+        String translation = Arrays.stream(src.split("[^\\p{Alpha}'`]"))
+                .map(new PigLatinTranslator()::translate)
+                .collect(Collectors.joining(" "));
+        if (Pattern.matches(".*?\\p{P}$", src)) {
+            // Matches anything followed by a punctuation character
+            return translation + src.charAt(src.length() - 1);
         }
-        if (src.isEmpty()) {
-            return src;
-        }
-        if (src.charAt(0) == '.')
-            return ".";
-
-
-        int length = src.length();
-
-        boolean addSentenceEnd = false;
-        //System.out.println("");
-        //System.out.println(src);
-        if (src.charAt(length - 1) == '.') {
-            System.out.println("in loop!");
-            src = src.substring(0, src.length() - 1);
-            addSentenceEnd = true;
-        }
-        //System.out.println("");
-        //System.out.println(src);
-        String[] words = src.split(" ");
-
-
-        String strOutput = "";
-        PigLatinTranslator plt = new PigLatinTranslator();
-        for (int i = 0; i < words.length; i++) {
-            // You may want to check for a non-word character before blindly
-            // performing a replacement
-            // It may also be necessary to adjust the character class
-            strOutput = strOutput.concat(plt.translate(words[i])).concat(" ");
-
-
-            //words[i] = words[i].replaceAll("[^\\w]", "");
-        }
-
-        strOutput = strOutput.trim();
-        if (addSentenceEnd)
-            strOutput = strOutput.concat(".");
-        //System.out.println("Result: " + strOutput);
-        return strOutput.trim();
+        return translation;
     }
 }
--- src/main/java/piglatin/SentenceReader.java
+++ src/main/java/piglatin/SentenceReader.java
@@ -6,19 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SentenceReader {
 
     public List<String> read(InputStream stream) throws IOException {
         List<String> sentences = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
-            while (true) {
-                String sentence = br.readLine();
-                if (sentence == null) {
-                    return sentences;
-                }
-                sentences.add(sentence);
-            }
+            return br.lines().collect(Collectors.toList());
         }
     }
 
