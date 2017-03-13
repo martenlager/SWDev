789909049548605acc7379d88f4d53fc8c204c9a
Rasmus Ros <rasmus.s.ros@gmail.com>
2017-03-13 08:33:40 +0100

    Changed built in java assert to jUnit asserts, so that any error messages are informative.

--- src/test/java/piglatin/ScoreTest.java
+++ src/test/java/piglatin/ScoreTest.java
@@ -9,49 +9,48 @@ public class ScoreTest {
     @Test
     public void correctWord() {
         Score score = new Score();
-        assert(score.score("Ellohay", "Ellohay") == 1);
+        assertEquals(1, score.score("Ellohay", "Ellohay"));
     }
-   
+
     // Test incorrect answer of single word 
     @Test
     public void incorrectWord() {
         Score score = new Score();
-        assert(score.score("Ellohay", "Helloay") == 0);
+        assertEquals(0, score.score("Ellohay", "Helloay"));
     }
 
     // Test that case sensitivity works
     @Test
     public void incorrectCase() {
         Score score = new Score();
-        assert(score.score("Ellohay", "ellohay") == 0);
-        assert(score.score("Ellohay", "ElLohay") == 0);
+        assertEquals(0, score.score("Ellohay", "ellohay"));
+        assertEquals(0, score.score("Ellohay", "ElLohay"));
     }
 
     // Test that things don't break on empty input
     @Test
     public void emptyInput() {
         Score score = new Score();
-        assert(score.score("Ellohay", "") == 0);
-        assert(score.score("Ellohay", null) == 0);
+        assertEquals(0, score.score("Ellohay", ""));
+        assertEquals(0, score.score("Ellohay", null));
     }
 
     // Empty solution is a bug
-    @Test(expected=NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void nullSolution() {
         Score score = new Score();
-        assert(score.score(null, "") == 0);
+        assertEquals(0, score.score(null, ""));
     }
 
     // Test that the score accumulates in the class on repeated scoring requests
     @Test
     public void accumulate() {
-      Score score = new Score();
-      assert(score.getScore() == 0);
-      assert(score.score("a", "a") == 1);
-      assert(score.score("b", "b") == 1);
-      assert(score.score("c", "c") == 1);
-      assert(score.score("d", "a") == 0);
-      assert(score.getScore() == 3);
+        Score score = new Score();
+        assertEquals(0, score.getScore());
+        assertEquals(1, score.score("a", "a"));
+        assertEquals(1, score.score("b", "b"));
+        assertEquals(1, score.score("c", "c"));
+        assertEquals(0, score.score("d", "a"));
+        assertEquals(3, score.getScore());
     }
-  
 }
